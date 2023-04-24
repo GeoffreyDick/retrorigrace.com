a386a3d0e23536529a0eed9cdda94cb80a2539ed
GeoffreyDick <git@geoffdick.com>
2023-04-24 07:42:06 -0600

    feat: add eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    "plugin:astro/recommended",
+  ],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+        // https://github.com/ota-meshi/eslint-plugin-astro
+      },
+    },
+  ],
+};
